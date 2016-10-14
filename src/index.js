932f8bf329bc2af3b57c10f61583ce7a0059133f
Jason Skomorowski <jason@indexdata.com>
2016-10-13 20:43:52 -0400

    Convert back to Webpack loader, first working iteration of new API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import assert from 'assert';
 import _ from 'lodash';
+// TODO find a simpler/more robust serialisation tool to do "JSON.stringify()
+// that leaves functions bare so eval() can regurgitate them eg. when used as
+// a Webpack loader"
+import Exval from 'exval';
+const exval = new Exval();
 
 const appendOrSingleton = (maybeArray, newValue) => {
   const singleton = [newValue];
@@ -9,11 +14,12 @@ const appendOrSingleton = (maybeArray, newValue) => {
   return singleton;
 };
 
-module.exports = (config, validators = {}) => {
-  assert(_.isObject(config) && _.isObject(config.modules),
-         'stripes-loader requires an object with the modules to enable as keys');
-  const enabled = config.modules;
-  const output = Object.assign({}, config);
+// Would use => here but apparently the implicit "this" binding also somehow
+// prevents rebinding? I need to bind() this to test it.
+module.exports = function stripesLoader() {
+  assert(_.isObject(this.options.stripesLoader), 'stripes-loader requires an object with the modules to enable as keys at the webpack configuration key "stripesLoader"');
+  const enabled = this.options.stripesLoader.modules;
+  const output = Object.assign({}, this.options.stripesLoader);
   delete output.modules;
   output.modules = {};
   _.forOwn(enabled, (overrides, stripe) => {
@@ -26,12 +32,15 @@ module.exports = (config, validators = {}) => {
            `included module ${stripe} does not specify stripes.type in package.json`));
     const stripeConfig = Object.assign({}, defaults, overrides, {
       module: stripe,
+      // TODO make this async ie. return a promise instead once we can handle that
+      // TODO yes, this is silly, but we can't serialize the closure
+      getModule: eval(`() => require('${stripe}').default`),
       moduleRoot: path.dirname(packageJSONPath),
-      modulePackageJSON: packageJSON,
+      description: packageJSON.description,
+      version: packageJSON.version,
     });
     delete stripeConfig.type;
     output.modules[defaults.type] = appendOrSingleton(output.modules[defaults.type], stripeConfig);
   });
-  console.log(output.modules);
-  return output;
+  return `module.exports = ${exval.stringify(output)}`;
 };
--- test/test.js
+++ test/test.js
@@ -2,41 +2,47 @@ import StripesLoader from '../src/index';
 import chai from 'chai';
 chai.should();
 
-const input = {
-  okapi: {
-    url: 'http://localhost/'
-  },
-  modules: {
-    '../test/test-module': {
-      a: 1 
+const mockWebpackThis = {
+  options: {
+    stripesLoader: {
+      okapi: {
+        url: 'http://localhost/'
+      },
+      modules: {
+        '../test/test-module': {
+          a: 1 
+        }
+      }
     }
   }
 };
 
 describe('StripesLoader', () => {
+
   it('should be a function', () => {
     StripesLoader.should.be.a('function');
   });
-  const output = StripesLoader(input);
-  it('should return an object with an array at output.modules.app', () => {
-    output.should.be.a('object');
-    output.modules.should.be.a('object');
-    output.modules.app.should.be.a('array');
+
+  const output = StripesLoader.bind(mockWebpackThis)();
+
+  it('should return a string', () => {
+    output.should.be.a('string');
   });
-  it('should override app.a', () => {
-    output.modules.app[0].a.should.equal(1);
+
+  eval(output);
+
+  it('output should evalute to an object called "module"', () => {
+    module.should.be.a('object');
+  });
+
+  const app = module.exports.modules.app[0];
+
+  it('module.exports.modules.app[0].getModule() should load main', () => {
+    app.getModule.should.be.a('function');
+    app.getModule().test.should.equal('test');
+  });
+
+  it('should override app[0].a', () => {
+    app.a.should.equal(1);
   });
 });
-
-const t = {
-  okapi: {
-    url: 'http://localhost/'
-  },
-  modules: {
-    '@folio-sample-modules/trivial': {
-      a: 1 
-    }
-  }
-};
-
-StripesLoader(t);
